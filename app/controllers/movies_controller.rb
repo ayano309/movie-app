00f10686726cea1ca2fb2644d71a248943e110ca
綾乃 <>
2022-04-08 19:13:02 +0900

    movieモデルのnew,indexページ作成

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,50 @@
 class MoviesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @movies = current_user.movies
+
   end
 
   def show
+    @movie = Movie.find(params[:id])
   end
 
   def new
+    @movie = current_user.movies.build
+  end
+
+  def create
+    @movie =current_user.movies.build(movie_params)
+    if @movie.save
+      redirect_to movie_path(@movie), notice: 'successfully created'
+    else
+      render :new
+    end
   end
 
   def edit
+    @movie = current_user.movies.find(params[:id])
+  end
+
+  def update
+    @movie = current_user.movies.find(movie_params)
+    if @movie.update
+      redirect_to movie_path(@movie), notice: 'successfully updated'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    movie = current_user.movies.find(params[:id])
+    movie.destroy!
+    redirect_to movies_path, notice: 'successfully Deleted'
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:date, :title, :appearance, :director, :body)
   end
 end
