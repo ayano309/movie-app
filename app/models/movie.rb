3eada4e6ac7fd855577ae5db5d9fc7c7d0b12d7a
ayano309 <ayatomato.k@gmail.com>
2022-07-23 17:29:50 +0900

    [add]アソシエーション

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,7 @@ class Movie < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :meals, dependent: :destroy
   has_one_attached :image
   validates :title, presence: true
   validates :body, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   has_many :movies, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :meals, dependent: :destroy
 end
